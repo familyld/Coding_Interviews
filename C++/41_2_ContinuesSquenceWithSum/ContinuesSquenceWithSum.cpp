d7ef786f0d4375159fc56f543c196366bd335864
familyld <family_ld@foxmail.com>
2016-08-17 20:30:19 +0800

    面试题41_2：和为s的连续正数序列

---
+++ C++/41_2_ContinuesSquenceWithSum/ContinuesSquenceWithSum.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+using namespace std;
+
+void PrintContinuousSequence(int small, int big);
+
+void FindContinuousSequence(int sum)
+{
+    if(sum < 3)
+        return;
+
+    int small = 1;
+    int big = 2;
+    int middle = (1 + sum) / 2;
+    int curSum = small + big;
+
+    while(small < middle)
+    {
+        if(curSum == sum)
+            PrintContinuousSequence(small, big);
+
+        while(curSum > sum && small < middle)
+        {
+            curSum -= small;
+            small ++;
+
+            if(curSum == sum)
+                PrintContinuousSequence(small, big);
+        }
+
+        big ++;
+        curSum += big;
+    }
+}
+
+void PrintContinuousSequence(int small, int big)
+{
+    for(int i = small; i <= big; ++ i)
+        printf("%d ", i);
+
+    printf("\n");
+}
+
+// ====================²âÊÔ´úÂë====================
+void Test(char* testName, int sum)
+{
+    if(testName != NULL)
+        printf("%s for %d begins: \n", testName, sum);
+
+    FindContinuousSequence(sum);
+    printf("\n");
+}
+
+int main()
+{
+    Test("test1", 1);
+    Test("test2", 3);
+    Test("test3", 4);
+    Test("test4", 9);
+    Test("test5", 15);
+    Test("test6", 100);
+
+    return 0;
+}
+
