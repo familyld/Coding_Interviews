58af410041044b6ad2d5d41e2e55988b61a701cf
familyld <family_ld@foxmail.com>
2016-08-19 20:59:48 +0800

    面试题47：不用加减乘除做加法

---
+++ C++/47_AddTwoNumbers/AddTwoNumbers.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+using namespace std;
+
+int Add(int num1, int num2)
+{
+    int sum, carry;
+    do
+    {
+        sum = num1 ^ num2;
+        carry = (num1 & num2) << 1;
+
+        num1 = sum;
+        num2 = carry;
+    }
+    while(num2 != 0);
+
+    return num1;
+}
+
+// ====================²âÊÔ´úÂë====================
+void Test(int num1, int num2, int expected)
+{
+    int result = Add(num1, num2);
+    if(result == expected)
+        printf("%d + %d is %d. Passed\n", num1, num2, result);
+    else
+        printf("%d + %d is %d. Failed\n", num1, num2, result);
+    printf("\n");
+}
+
+int main()
+{
+    Test(1, 2, 3);
+    Test(111, 899, 1010);
+
+    Test(-1, 2, 1);
+    Test(1, -2, -1);
+
+    Test(3, 0, 3);
+    Test(0, -4, -4);
+
+    Test(-2, -8, -10);
+}
+
